3ba214346664f3b647704e6ff814f0960a38af90
Zachary Sunberg <sunbergzach@gmail.com>
2017-10-11 18:20:27 -0700

    started going down the storage road

--- src/random_2.jl
+++ src/random_2.jl
@@ -5,22 +5,24 @@ import Base.Random.MTCacheLength
 mutable struct MemorizingSource{RNG<:AbstractRNG} <: DESPOTRandomSource
     rng::RNG
     memory::Vector{Float64}
-    ranges::Matrix{UnitRange{Int64}}
+    rngs::Matrix{MemorizingRNG{MemorizingSource{RNG}}}
     furthest::Int
 end
 
 function MemorizingSource(K::Int, depth::Int, rng::AbstractRNG)
+    RNG = typeof(rng)
     memory = Float64[]
-    ranges = fill(0:0, depth, K)
-    return MemorizingSource{typeof(rng)}(rng, memory, ranges, 0)
+    rngs = Matrix{MemorizingRNG{MemorizingSource{RNG}}}(depth, K)
+    return MemorizingSource{RNG}(rng, memory, rng, 0)
 end
 
 function get_rng(s::MemorizingSource, scenario::Int, depth::Int)
+    if isassigned(s.rngs, depth+1, scenario)
+        rng = s.rngs
+    else
     range = s.ranges[depth+1, scenario]
-    new = false
     if range == 0:0
         range = s.furthest+1:s.furthest
-        new = true
     end
     return MemorizingRNG(s.memory, range, 0, s)
 end
@@ -49,17 +51,3 @@ function gen_rand!(r::MemorizingRNG{MemorizingSource{MersenneTwister}}, n::Integ
     end
     return nothing
 end
-
-#=
-function gen_one!(r::MemorizingRNG{S}) where S<: MemorizingSource
-    s = r.source
-    if r === s.head
-        push!(s.memory, rand(s.rng, Close1Open2))
-        r.vals = view(s.memory, first(first(r.vals.indexes)):length(r.vals))
-    else
-        error("tried to gen_rand on an rng that is not the head")
-    end
-    return nothing
-
-end
-=#
