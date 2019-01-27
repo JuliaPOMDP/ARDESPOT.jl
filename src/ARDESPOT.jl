eac99a80d2c30c17c0de0fc5420bc66775a7c006
Zachary Sunberg <sunbergzach@gmail.com>
2019-01-26 23:20:48 -0800

    field docstrings

--- src/ARDESPOT.jl
+++ src/ARDESPOT.jl
@@ -45,68 +45,71 @@ export
 include("random_2.jl")
 
 """
-    DESPOTSolver
+    DESPOTSolver(<keyword arguments>)
 
-    An implementation of the ARDESPOT solver trying to closely match the pseudeo
-    code of:
+Implementation of the ARDESPOT solver trying to closely match the pseudo code of:
 
-    http://bigbird.comp.nus.edu.sg/m2ap/wordpress/wp-content/uploads/2017/08/jair14.pdf
+http://bigbird.comp.nus.edu.sg/m2ap/wordpress/wp-content/uploads/2017/08/jair14.pdf
 
+Each field may be set via keyword argument. The fields that correspond to algorithm
+parameters match the definitions in the paper exactly.
 
-    Fields:
+# Fields
+- epsilon_0
+- xi
+- K
+- D
+- lambda
+- T_max
+- max_trials
+- bounds
+- default_action
+- rng
+- random_source
+- bounds_warnings
+- tree_in_info
 
-    epsilon_0::Float64
-        The target gap between the upper and the lower bound at the root of the partial DESPOT.
-
-    xi::Float64
-        The rate of target gap reduction.
-
-    K::Int
-        The number of sampled scenarios.
-
-    D::Int
-        The maximum depth of the DESPOT.
-
-    lambda::Float64
-        Reguluarization constant.
-
-    T_max::Float64
-        The maximum online planning time per step.
-
-    max_trials::Int
-        The maximum number of trials of the planner.
-
-    bounds::Any
-        A representation for the upper and lower bound on the RWDU (e.g. IndependentBounds).
-
-    default_action::Any
-        A default action to be returned if the algorithm fails to provide an action.
-
-    rng::AbstractRNG
-        A random number generator for the internal sampling processes.
-
-    random_source::DESPOTRandomSource
-        A source for random numbers in scenario rollout
-
-    bounds_warnings::Bool
-        If true, sanity checks on the provided bounds are performed.
-
-    tree_in_info::Bool
-        If true, a reprenstation of the constructed DESPOT is returned by POMDPModelTools.action_info.
+Further information can be found in the field docstrings (e.g.
+`?DESPOTSolver.xi`)
 """
 @with_kw mutable struct DESPOTSolver <: Solver
+    "The target gap between the upper and the lower bound at the root of the partial DESPOT."
     epsilon_0::Float64                      = 0.0
+
+    "The rate of target gap reduction."
     xi::Float64                             = 0.95
+
+    "The number of sampled scenarios."
     K::Int                                  = 500
+
+    "The maximum depth of the DESPOT."
     D::Int                                  = 90
+
+    "Reguluarization constant."
     lambda::Float64                         = 0.01
+
+    "The maximum online planning time per step."
     T_max::Float64                          = 1.0
+
+    "The maximum number of trials of the planner."
     max_trials::Int                         = typemax(Int)
+
+    "A representation for the upper and lower bound on the discounted value (e.g. `IndependentBounds`)."
     bounds::Any                             = IndependentBounds(-1e6, 1e6)
+
+    "A default action to be returned if the algorithm fails to provide an action."
     default_action::Any                     = ExceptionRethrow()
+    
+    "A random number generator for the internal sampling processes."
     rng::AbstractRNG                        = Random.GLOBAL_RNG
+    
+    "A source for random numbers in scenario rollout"
     random_source::DESPOTRandomSource       = MemorizingSource(K, D, rng)
+    
+    "If true, sanity checks on the provided bounds are performed."
     bounds_warnings::Bool                   = true
+
+    "If true, a reprenstation of the constructed DESPOT is returned by POMDPModelTools.action_info."
     tree_in_info::Bool                      = false
 end
 
