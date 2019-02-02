0cac56f1010cd6bcc25dbbdf2972e0fd0392e679
Zachary Sunberg <sunbergzach@gmail.com>
2019-02-02 11:39:52 -0800

    added backticks to docstring for formatting

--- src/ARDESPOT.jl
+++ src/ARDESPOT.jl
@@ -55,19 +55,19 @@ Each field may be set via keyword argument. The fields that correspond to algori
 parameters match the definitions in the paper exactly.
 
 # Fields
-- epsilon_0
-- xi
-- K
-- D
-- lambda
-- T_max
-- max_trials
-- bounds
-- default_action
-- rng
-- random_source
-- bounds_warnings
-- tree_in_info
+- `epsilon_0`
+- `xi`
+- `K`
+- `D`
+- `lambda`
+- `T_max`
+- `max_trials`
+- `bounds`
+- `default_action`
+- `rng`
+- `random_source`
+- `bounds_warnings`
+- `tree_in_info`
 
 Further information can be found in the field docstrings (e.g.
 `?DESPOTSolver.xi`)
