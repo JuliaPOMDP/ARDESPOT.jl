0bf8b789cdd12af9d62b15778606d4479e83e750
Zachary Sunberg <sunbergzach@gmail.com>
2019-05-08 16:47:35 -0700

    fixed tests

--- test/independent_bounds.jl
+++ test/independent_bounds.jl
@@ -4,8 +4,8 @@
     sb = ScenarioBelief([1=>true], rs, 0, false)
     b = IndependentBounds(0.0, -1e-5)
     @test bounds(b, m, sb) == (0.0, -1e-5)
-    b = IndependentBounds(0.0, -1e-5, bound_correction_thresh=1e-5)
+    b = IndependentBounds(0.0, -1e-5, consistency_fix_thresh=1e-5)
     @test bounds(b, m, sb) == (0.0, 0.0)
-    b = IndependentBounds(0.0, -1e-4, bound_correction_thresh=1e-5)
+    b = IndependentBounds(0.0, -1e-4, consistency_fix_thresh=1e-5)
     @test bounds(b, m, sb) == (0.0, -1e-4)
 end
