38720a59d2f9a7ee6d212f4f7b379fee0066d9d1
Zachary Sunberg <sunbergzach@gmail.com>
2020-07-20 11:08:53 -0700

    Update bounds.jl

--- src/bounds.jl
+++ src/bounds.jl
@@ -22,15 +22,13 @@ function bounds_sanity_check(pomdp::POMDP, sb::ScenarioBelief, L_0, U_0)
 end
 
 """
-    IndependentBounds(l, u, check_terminal=false, consistency_fix_thresh=0.0)
+    IndependentBounds(lower, upper, check_terminal=false, consistency_fix_thresh=0.0)
 
 Specify lower and upper bounds that are independent of each other (the most common case).
 
-This differs from specifying bounds as a `Tuple` because of the keyword options.
-
 # Keyword Arguments
 - `check_terminal::Bool=false`: if true, then if all the states in the belief are terminal, the upper and lower bounds will be overridden and set to 0.
-- `consistency_fix_thresh::Float64=0.0`: if `u < l` and `u >= l-consistency_fix_thresh`, then `u` will be bumped up to `l`.
+- `consistency_fix_thresh::Float64=0.0`: if `upper < lower` and `upper >= lower-consistency_fix_thresh`, then `upper` will be bumped up to `lower`.
 """
 struct IndependentBounds{L, U}
     lower::L
