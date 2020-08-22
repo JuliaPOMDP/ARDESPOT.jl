edafcafdfdbb6870348382197b6f1e3a4264a046
Zachary Sunberg <zachary.sunberg@colorado.edu>
2020-08-22 13:42:29 -0600

    fixed rng

--- src/memorizing_rng.jl
+++ src/memorizing_rng.jl
@@ -1,4 +1,4 @@
-import Random: CloseOpen01_64, CloseOpen12_64, FloatInterval, MT_CACHE_F
+using Random: CloseOpen12, CloseOpen01_64, CloseOpen12_64, FloatInterval, MT_CACHE_F, SamplerUnion, UInt52Raw, SamplerTrivial, BitInteger, SamplerType
 
 mutable struct MemorizingRNG{S} <: AbstractRNG
     memory::Vector{Float64}
@@ -11,24 +11,37 @@ end
 MemorizingRNG(source) = MemorizingRNG(Float64[], 1, 0, 0, source)
 
 # Low level API (copied from MersenneTwister)
-@inline mr_avail(r::MemorizingRNG) = r.finish - r.idx
-@inline mr_empty(r::MemorizingRNG) = r.idx == r.finish
-@inline mr_pop!(r::MemorizingRNG) = @inbounds return r.memory[r.idx+=1]
+mr_avail(r::MemorizingRNG) = r.finish - r.idx
+mr_empty(r::MemorizingRNG) = r.idx == r.finish
+mr_pop!(r::MemorizingRNG) = @inbounds return r.memory[r.idx+=1]
 
-@inline reserve_1(r::MemorizingRNG) = mr_empty(r) && gen_rand!(r, 1)
-@inline reserve(r::MemorizingRNG, n::Integer) = mr_avail(r) < n && gen_rand!(r, n)
+reserve_1(r::MemorizingRNG) = mr_empty(r) && gen_rand!(r, 1)
+reserve(r::MemorizingRNG, n::Integer) = mr_avail(r) < n && gen_rand!(r, n)
 
 # precondition: !mr_empty(r)
-@inline rand_inbounds(r::MemorizingRNG, ::Type{CloseOpen12_64}) = mr_pop!(r)
-@inline rand_inbounds(r::MemorizingRNG, ::Type{CloseOpen01_64}) = rand_inbounds(r, CloseOpen12_64) - 1.0
-@inline rand_inbounds(r::MemorizingRNG) = rand_inbounds(r, CloseOpen01_64)
+rand_inbounds(r::MemorizingRNG, ::CloseOpen12_64) = mr_pop!(r)
+rand_inbounds(r::MemorizingRNG, ::CloseOpen01_64) = rand_inbounds(r, CloseOpen12_64()) - 1.0
+rand_inbounds(r::MemorizingRNG, ::UInt52Raw{T}) where {T<:BitInteger} = reinterpret(UInt64, rand_inbounds(r, CloseOpen12())) % T
+rand_inbounds(r::MemorizingRNG) = rand_inbounds(r, CloseOpen01_64())
 
 Random.rng_native_52(rng::MemorizingRNG) = Random.rng_native_52(rng.source)
 
 function rand(r::MemorizingRNG, ::Union{I, Type{I}}) where I <: FloatInterval
     reserve_1(r)
-    rand_inbounds(r, I)
+    rand_inbounds(r, I())
 end
+
+function rand(r::MemorizingRNG, x::SamplerTrivial{UInt52Raw{UInt64}})
+    reserve_1(r)
+    rand_inbounds(r, x[])
+end
+
+rand(r::MemorizingRNG, T::SamplerUnion(Bool, Int8, UInt8, Int16, UInt16, Int32, UInt32)) =
+    rand(r, UInt52Raw()) % T[]
+
+# zsunberg made the two below up
+rand(r::MemorizingRNG, T::SamplerType{UInt64}) = rand(r, UInt32)*(convert(UInt64, typemax(UInt32)) + one(UInt64)) + rand(r, UInt32)
+rand(r::MemorizingRNG, T::SamplerType{Int64}) = reinterpret(Int64, rand(r, UInt64))
 
 # rand(r::MemorizingRNG, ::Type{Float64}) = rand(r, CloseOpen01)
 
--- test/memorizing_rng.jl
+++ test/memorizing_rng.jl
@@ -7,8 +7,11 @@ rng = MemorizingRNG(mt)
 
 rand(rng)
 rand(rng, Float64)
+rand(rng, Int32)
 rand(rng, [:a, :b, :c])
-# rand(rng, Int) # XXX this doesn't work
+rand(rng, Int)
 randn(rng)
 rand(rng, 1:1_000_000)
 randn(rng, (3,3))
+
+@test isapprox(sum(rand(rng, Bool, 1_000_000))/1_000_000, 0.5, atol=0.001)
